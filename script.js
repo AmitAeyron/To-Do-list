49dcd5c472c704a5714f15b6e78e67c03a248733
Amit Kumar <99206503+AmitAeyron@users.noreply.github.com>
2023-05-20 14:49:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function addTask() {
+    var taskInput = document.getElementById('taskInput');
+    var taskList = document.getElementById('taskList');
+  
+    if (taskInput.value !== '') {
+      var task = document.createElement('li');
+      task.innerText = taskInput.value;
+  
+      task.addEventListener('click', function() {
+        task.classList.toggle('completed');
+      });
+  
+      taskList.appendChild(task);
+      taskInput.value = '';
+    }
+  }
+  
